eea0b28a68454c08e2dea01417ce8f1d273aa191
Mário Idival <marioidival@gmail.com>
2021-07-08 23:15:48 -0300

    fix retreive database info on fiat repository

--- adapters/postgres/fiat.go
+++ adapters/postgres/fiat.go
@@ -26,7 +26,7 @@ func (f *FiatPricesRepository) GetFiatPrices(ctx context.Context, currency strin
 
 	for stmt.Next() {
 		fp := domain.FiatPrice{}
-		err = stmt.Scan(fp.Currency, fp.BaseCurrency, fp.Price)
+		err = stmt.Scan(&fp.Currency, &fp.BaseCurrency, &fp.Price)
 		if err != nil {
 			return fiatPrices, err
 		}
@@ -39,7 +39,7 @@ func (f *FiatPricesRepository) GetFiatPrices(ctx context.Context, currency strin
 func (f *FiatPricesRepository) CreateFiatPrice(ctx context.Context, base_currency, currency string, price float64) error {
 	stmt, err := f.conn.db.PrepareContext(ctx, "INSERT INTO fiat(currency, base_currency, price) VALUES ($1, $2, $3)")
 	if err != nil {
-		return nil
+		return err
 	}
 	_, err = stmt.ExecContext(ctx, currency, base_currency, price)
 	return err
@@ -48,7 +48,7 @@ func (f *FiatPricesRepository) CreateFiatPrice(ctx context.Context, base_currenc
 func (f *FiatPricesRepository) UpdateFiatPrice(ctx context.Context, base_currency, currency string, price float64) error {
 	stmt, err := f.conn.db.PrepareContext(ctx, "UPDATE fiat SET price = $1 WHERE currency = $2 AND base_currency = $3")
 	if err != nil {
-		return nil
+		return err
 	}
 	_, err = stmt.ExecContext(ctx, price, currency, base_currency)
 	return err
