3d220f9276b6600ec920a4258fcb89a0bcddb1b9
Mário Idival <marioidival@gmail.com>
2021-07-08 21:42:24 -0300

    add adapter to postgresql

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import "github.com/spf13/viper"
 type Config struct {
 	Mongo      MongoConfig
 	HTTPServer HTTPServerConfig
+	Postgres   PostgresConfig
 }
 
 type MongoConfig struct {
@@ -13,6 +14,17 @@ type MongoConfig struct {
 	Host     string
 	Port     int
 	Database string
+}
+
+type PostgresConfig struct {
+	User           string
+	Password       string
+	Host           string
+	Port           int
+	Database       string
+	SslModeEnabled bool
+	MaxIdleConns   int
+	MaxOpenConns   int
 }
 
 type HTTPServerConfig struct {
@@ -43,6 +55,16 @@ func Load() Config {
 		HTTPServer: HTTPServerConfig{
 			Host: viper.GetString("HTTP_HOST"),
 			Port: viper.GetInt("HTTP_PORT"),
+		},
+		Postgres: PostgresConfig{
+			User:           viper.GetString("POSTGRES_USER"),
+			Password:       viper.GetString("POSTGRES_PASSWORD"),
+			Host:           viper.GetString("POSTGRES_HOST"),
+			Port:           viper.GetInt("POSTGRES_PORT"),
+			Database:       viper.GetString("POSTGRES_DATABASE"),
+			SslModeEnabled: viper.GetBool("POSTGRES_SSL_ENABLED"),
+			MaxIdleConns:   viper.GetInt("POSTGRES_MAX_ID_CONNS"),
+			MaxOpenConns:   viper.GetInt("POSTGRES_MAX_OPEN_CONNS"),
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require (
 	github.com/dghubble/sling v1.3.0
 	github.com/gofiber/fiber/v2 v2.14.0
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/spf13/viper v1.8.1
 	github.com/valyala/fasthttp v1.28.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.4
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
