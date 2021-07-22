da4c48fe0b084c3dc2ab9257522744b584ed6cb4
Mário Idival <marioidival@gmail.com>
2021-07-22 20:30:54 -0300

    Create Project Repository

---
+++ adapters/bbolt/config.go
@@ -0,0 +1,60 @@
+package bbolt
+
+import "github.com/hermeznetwork/price-updater-service/core/ports"
+
+type ProjectConfigRepository struct {
+	db *Connection
+}
+
+type projectConfigModels struct {
+	Origins string `json:"origins"`
+}
+
+func NewProjectConfigRepository(db *Connection) ports.ProjectConfigRepository {
+	return &ProjectConfigRepository{
+		db: db,
+	}
+
+}
+
+func (pc *ProjectConfigRepository) SaveAllowedOrigin(origins string) error {
+	pc.db.Start()
+	defer pc.db.End()
+
+	tx, err := pc.db.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	bkt, err := tx.CreateBucketIfNotExists([]byte("projectConfig"))
+	if err != nil {
+		return err
+	}
+
+	err = bkt.Put([]byte("origins"), []byte(origins))
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func (pc *ProjectConfigRepository) LoadAllowedOrigin() (string, error) {
+	pc.db.Start()
+	defer pc.db.End()
+	tx, err := pc.db.db.Begin(true)
+	if err != nil {
+		return "", nil
+	}
+	defer tx.Rollback()
+	bkt, err := tx.CreateBucketIfNotExists([]byte("projectConfig"))
+	if err != nil {
+		return "", err
+	}
+
+	originsRaw := bkt.Get([]byte("origins"))
+	if originsRaw == nil {
+		return "*", tx.Commit()
+	}
+	return string(originsRaw), tx.Commit()
+}
